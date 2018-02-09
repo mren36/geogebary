291d998acb2eea5d48615aa7085d6be56785741c
Circador <ericyou00@yahoo.com>
2018-02-09 14:30:48 -0500

    Added javadocs to Line. Working on Point javadocs.

--- Point.java
+++ Point.java
@@ -1,13 +1,18 @@
-// a class that represents a point with barycentric coordinates (x : y : z) for some HomogenousVector (x, y, z)
-
 import java.awt.*;
 import javax.swing.*;
 import java.math.*;
 
+/**
+ * a class that represents a point with barycentric coordinates (x : y : z) for some HomogenousVector (x, y, z)
+ */
 public class Point
 {
   private HomogenousVector coords; // stores the coordinates of the point
 
+  /**
+   *
+   * @param coords
+   */
   public Point(HomogenousVector coords) // sets the coordinates of the point
   {
     if (coords.equalsZero())
@@ -17,6 +22,12 @@ public class Point
     this.coords = v;
   }
 
+  /**
+   *
+   * @param x
+   * @param y
+   * @param z
+   */
   public Point(HomogenousPolynomial x, HomogenousPolynomial y, HomogenousPolynomial z) // sets the coordinates of the point
                                                                                        // by inputting polynomials
   {
@@ -28,6 +39,12 @@ public class Point
     this.coords = v;
   }
 
+  /**
+   *
+   * @param xString
+   * @param yString
+   * @param zString
+   */
   public Point(String xString, String yString, String zString) // sets the coordinates of the opint by inputting Strings
   {
     coords = new HomogenousVector(xString, yString, zString);
@@ -38,6 +55,11 @@ public class Point
     this.coords = v;
   }
 
+  /**
+   *
+   * @param l1
+   * @param l2
+   */
   public Point(Line l1, Line l2) // returns the interesection of two lines, determined by the cross product of their coefficients
   {
     if (l1.equals(l2))
@@ -47,66 +69,124 @@ public class Point
     this.coords = v;
   }
 
+  /**
+   *
+   * @param other
+   */
   public Point(Point other) // copy constructor
   {
     coords = other.coords;
   }
 
+  /**
+   *
+   * @param other
+   * @return
+   */
   public boolean equals(Point other) // returns whether other has the same coordinates
   {
     return coords.equals(other.coords);
   }
 
+  /**
+   *
+   * @return
+   */
   public HomogenousVector getCoords() // getter for coords
   {
     return coords;
   }
 
+  /**
+   *
+   * @return
+   */
   public String toString() // returns String format of the point coordinates
   {
     return "( " + coords.getX() + " : " + coords.getY() + " : " + coords.getZ() + " )";
   }
 
+  /**
+   *
+   * @param other
+   * @return
+   */
   public Line interpolate(Point other) // returns line through this and other
   {
     return new Line(this, other);
   }
 
+  /**
+   *
+   * @param l
+   * @return
+   */
   public boolean on(Line l) // returns whether this is on line l, checks by dot product of coordinates and coefficients
   {
     return coords.perp(l.getCoeffs());
   }
 
+  /**
+   *
+   * @param other
+   * @return
+   */
   public Point reflectOver(Point other) // returns the reflection of this over other
   {
     return Geometry.reflect(this, other);
   }
 
+  /**
+   *
+   * @param l
+   * @return
+   */
   public Point reflectOver(Line l) // returns the reflection of the point over l
   {
     return Geometry.reflect(this, l);
   }
 
+  /**
+   *
+   * @return
+   */
   public HomogenousPolynomial weight() // returns the weight of the point, i.e. the sum of its coordinates
   {
     return coords.weight();
   }
 
+  /**
+   *
+   * @return
+   */
   public HomogenousPolynomial getX() // getter for x coordinate
   {
     return coords.getX();
   }
 
+  /**
+   *
+   * @return
+   */
   public HomogenousPolynomial getY() // getter for y coordinate
   {
     return coords.getY();
   }
 
+  /**
+   *
+   * @return
+   */
   public HomogenousPolynomial getZ() // getter for z coordinate
   {
     return coords.getZ();
   }
 
+  /**
+   *
+   * @param c
+   * @return
+   */
   public boolean on(Circle c) // returns whether this is on c, checks by plugging coordinates into circle equation
   {
     HomogenousPolynomial pow = c.getCoeff().times(new HomogenousPolynomial("a^2").times(coords.getY().times(coords.getZ())).plus(new HomogenousPolynomial("b^2").times(coords.getZ().times(coords.getX()))).plus(new HomogenousPolynomial("c^2").times(coords.getX().times(coords.getY()))));
@@ -114,6 +194,16 @@ public class Point
     return pow.equals(rad);
   }
 
+  /**
+   *
+   * @param ax
+   * @param ay
+   * @param bx
+   * @param by
+   * @param cx
+   * @param cy
+   * @return
+   */
   public int[] screenCoords(int ax, int ay, int bx, int by, int cx, int cy) // draws the point in g given coordinates of triangle
   {
     int a = (int) Math.round(Math.sqrt((bx - cx) * (bx - cx) + (by - cy) * (by - cy)));
@@ -132,49 +222,104 @@ public class Point
     return sC;
   }
 
+  /**
+   *
+   * @param c
+   * @return
+   */
   public Line polar(Circle c)
   {
     return c.polar(this);
   }
 
   // Simplified average Method
+
+  /**
+   *
+   * @param P2
+   * @param w1
+   * @param w2
+   * @return
+   */
   public Point average(Point P2, int w1, int w2)
   {
     return Geometry.average(this, P2, w1, w2);
   }
 
   // Simplified midpoint Method
+
+  /**
+   *
+   * @param P2
+   * @return
+   */
   public Point midpoint(Point P2) // returns the midpoint of P1 and P2
   {
     return Geometry.midpoint(this, P2);
   }
 
   // Simplified reflect Method
+
+  /**
+   *
+   * @param P2
+   * @return
+   */
   public Point reflect(Point P2) // returns the reflection of P1 over P2
   {
     return Geometry.reflect(this, P2);
   }
 
+  /**
+   *
+   * @param P2
+   * @param w1
+   * @param w2
+   * @return
+   */
   public Point polyAverage(Point P2, HomogenousPolynomial w1, HomogenousPolynomial w2)
   {
     return Geometry.polyAverage(this, P2, w1, w2);
   }
 
+  /**
+   *
+   * @param P1
+   * @param P2
+   * @return
+   */
   public Point foot(Point P1, Point P2) // returns the foot from P to the line through P1 and P2
   {
     return Geometry.foot(this, P1, P2);
   }
 
+  /**
+   *
+   * @param P2
+   * @param P3
+   * @return
+   */
   public Point centroid(Point P2, Point P3) // returns the centroid of triangle P1P2P3
   {
     return Geometry.centroid(this, P2, P3);
   }
 
+  /**
+   *
+   * @param P1
+   * @param P2
+   * @param P3
+   * @return
+   */
   public Point circumcenter(Point P1, Point P2, Point P3)
   {
     return Geometry.circumcenter(this, P2, P3);
   }
 
+  /**
+   *
+   * @return
+   */
   public int hashCode()
   {
     return coords.hashCode();
